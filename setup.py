b6b89d9873ebdf9d7ab864bc41881117fe419d12
Matthew Trentacoste <web+git@matttrent.com>
2023-01-01 14:29:36 -0800

    updating requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,26 +4,34 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='recreation',
-   version='2.0',
-   description='A recreation.gov campground and permit API',
-   license="MIT",
-   long_description=long_description,
-   author='Matthew Trentacoste',
-   author_email='web+git@matttrent.com',
-   url="https://github.com/matttrent/recreation-gov-campsite-checker",
-   packages=['recreation'],  #same as name
-   # external packages as dependencies
-   install_requires=[
-        "backoff>=2.1.2",
-        "api-client-pydantic>=2.2.0",
-        "colorama==0.4.5",
-        "fake-useragent==0.1.11",
-        "rich>=12.5.1",
-        "shellingham>=1.4.0",
-        "typer>=0.6.1",
-   ], 
-   scripts=[
+    name='recreation',
+    version='2.0',
+    description='A recreation.gov campground and permit API',
+    license="MIT",
+    long_description=long_description,
+    author='Matthew Trentacoste',
+    author_email='web+git@matttrent.com',
+    url="https://github.com/matttrent/recreation-checker",
+    packages=['recreation'],  #same as name
+    # external packages as dependencies
+    install_requires=[
+        "api-client>=1.3,<2.0",
+        "api-client-pydantic>=2.2,<3.0",
+        "backoff>=2.2,<3.0",
+        "click>=8.1,<9.0",
+        "colorama>=0.4,<1.0",
+        "fake-useragent>=1.1,<2.0",
+        "pydantic>=1.10,<2.0",
+        "regex>=2022.10",
+        "requests>=2.28.1,<3.0",
+        "responses>=0.12.3,<1.0",
+        "rich>=13.0,<14.0",
+        "shellingham>=1.5,<2.0",
+        "tenacity>=8.1,<9.0",
+        "typer>=0.7,<1.0",
+        "types-click>=7.1,<8.0",
+    ], 
+    scripts=[
         'scripts/camping.py',
     ],
 )
